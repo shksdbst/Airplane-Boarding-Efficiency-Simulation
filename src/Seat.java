22a2511aef1be890c366836f459d4235820213ba
Soohyeon Kim <bestsoohyeon@gmail.com>
2026-06-10 01:47:42 +0900

    Phase 1 (Passenger, Seats, timeline)

---
+++ src/Seat.java
@@ -0,0 +1,11 @@
+public class Seat {
+    int row;
+    int col;
+    boolean occupied;
+
+    public Seat(int row, int col) {
+        this.row = row;
+        this.col = col;
+        this.occupied = false;
+    }
+}
